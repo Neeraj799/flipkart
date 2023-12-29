fa6336afbb638bce1cd8a3973ac77f02cda28821
neeraj <neerajpc799@gmail.com>
2023-12-29 17:23:16 +0530

    add to cart

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Review(122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium nisi minima soluta veritatis, pariatur molestias laudantium inventore cum minus, perspiciatis mollitia consequatur repellendus eius? Ipsum asperiores rem veritatis blanditiis vel dicta in dolorum et? At omnis repellat esse iure rem autem quia impedit natus commodi, quod quibusdam iste nulla rerum!</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium fuga ut velit id dolor autem repellat molestiae aut, consequatur iure.</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
